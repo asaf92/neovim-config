80dff73600937155de768e2189b5c1587341b55f
asaf92-legit <asafa@legitsecurity.com>
2024-03-21 17:58:16 +0200

    add marks

--- lua/asaf/packer.lua
+++ lua/asaf/packer.lua
@@ -47,4 +47,25 @@ return require('packer').startup(function(use)
       {'rafamadriz/friendly-snippets'},
     }
   }
+  use {
+    'chentoast/marks.nvim',
+    config = function()
+      require'marks'.setup {
+        default_mappings = true,
+        builtin_marks = { ".", "<", ">", "^" },
+        cyclic = true,
+        force_write_shada = false,
+        refresh_interval = 250,
+        sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
+        excluded_filetypes = {},
+        excluded_buftypes = {},
+        bookmark_0 = {
+          sign = "⚑",
+          virt_text = "hello world",
+          annotate = false,
+        },
+        mappings = {}
+      }
+    end
+  }
 end)

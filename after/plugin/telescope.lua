4433c6184e37894025ab9889ae34af33360e20b7
aagami <asaf92@gmail.com>
2024-04-05 15:37:57 +0300

    use telescope-ui-select for code actions

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,16 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+-- Setup for telescope-ui-select extension
+require('telescope').setup{
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown { }
+      -- Additional configuration for the dropdown theme as needed
+    },
+  },
+}
+
+-- Load the ui-select extension
+require('telescope').load_extension('ui-select')
--- lua/asaf/packer.lua
+++ lua/asaf/packer.lua
@@ -8,19 +8,11 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use {'nvim-telescope/telescope-ui-select.nvim' }
 
   use 'Mofiqul/vscode.nvim' 
-
-  -- use({
-  --         'Mofiqul/vscode.nvim',
-  --         as = 'vscode',
-  --         config = function()
-  --       	  vim.cmd('colorscheme vscode')
-  --         end
-  -- })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('theprimeagen/harpoon')

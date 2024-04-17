29efa8aafd5c9cfac863b579c9964592826969fd
asaf92-legit <asafa@legitsecurity.com>
2024-04-17 15:26:15 +0300

    add hotkey to show diagnostic in popup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -53,8 +53,13 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  
+  -- diagnostics
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set('n', '<Leader>d', function() vim.diagnostic.open_float(nil, {focus=false}) end, opts)
+  
+  -- LSP actions
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)

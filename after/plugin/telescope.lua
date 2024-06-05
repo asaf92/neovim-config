4b1d581b28f1ba2068d7d7073db56270af87587b
asaf92-legit <asafa@legitsecurity.com>
2024-06-05 16:05:34 +0300

    integrate telescope with harpoon

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -17,3 +17,17 @@ require('telescope').setup{
 
 -- Load the ui-select extension
 require('telescope').load_extension('ui-select')
+
+-- Harpoon
+require('telescope').load_extension('harpoon')
+local function launch_harpoon_marks()
+  local opts = {
+    attach_mappings = function(_, map)
+      map("i", "<c-n>", "move_selection_next")
+      map("i", "<c-p>", "move_selection_previous")
+      return true
+    end,
+  }
+  require("telescope").extensions.harpoon.marks(opts)
+end
+vim.keymap.set('n', '<leader>e', launch_harpoon_marks, {})

e981513dd913074baacd264c9def088b1203222c
aagami <asaf92@gmail.com>
2024-06-12 02:00:37 +0300

    cool remap

--- lua/asaf/remap.lua
+++ lua/asaf/remap.lua
@@ -4,3 +4,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Center screen after scroll
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<leader>Y", "ggVGy", {noremap = true, silent = true })

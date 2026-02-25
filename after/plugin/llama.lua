5a816f697bebe3c18ccc5d2e69594321248467fa
Asaf Agami <asaf@geniesecurity.io>
2026-02-25 10:39:00 +0200

    add llama.vim

---
+++ after/plugin/llama.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('i', '<C-_>', 'llama#fim_inline(v:false, v:false)', {
+  expr = true,
+  silent = true,
+})
--- lua/asaf/packer.lua
+++ lua/asaf/packer.lua
@@ -39,6 +39,23 @@ return require('packer').startup(function(use)
   use 'rafamadriz/friendly-snippets'
   use 'stevearc/conform.nvim'
   use {
+    'ggml-org/llama.vim',
+    setup = function()
+      vim.g.llama_config = {
+        auto_fim = false,
+        endpoint_fim = 'http://127.0.0.1:8012/infill',
+        keymap_fim_trigger = '<C-f>',
+        keymap_fim_accept_full = '<Tab>',
+        keymap_fim_accept_line = '<S-Tab>',
+        keymap_inst_trigger = '',
+        keymap_inst_rerun = '',
+        keymap_inst_continue = '',
+        keymap_inst_accept = '',
+        keymap_inst_cancel = '',
+      }
+    end,
+  }
+  use {
     'chentoast/marks.nvim',
     config = function()
       require'marks'.setup {

10268922234499e077a095bcbef25e12aebabae4
Asaf Agami <asaf.agami@cyera.io>
2026-06-14 13:03:23 +0300

    rust lsp

--- after/plugin/vscode.lua
+++ after/plugin/vscode.lua
@@ -6,31 +6,44 @@ vim.o.background = 'dark'
 
 local c = require('vscode.colors').get_colors()
 require('vscode').setup({
-    -- Alternatively set style in setup
-    -- style = 'light'
+  -- Alternatively set style in setup
+  -- style = 'light'
 
-    -- Enable transparent background
-    transparent = false,
+  -- Enable transparent background
+  transparent = false,
 
-    -- Enable italic comment
-    italic_comments = true,
+  -- Enable italic comment
+  italic_comments = true,
 
-    -- Underline `@markup.link.*` variants
-    underline_links = false,
+  -- Underline `@markup.link.*` variants
+  underline_links = false,
 
-    -- Disable nvim-tree background color
-    disable_nvimtree_bg = false,
+  -- Disable nvim-tree background color
+  disable_nvimtree_bg = false,
 
-    -- Override colors (see ./lua/vscode/colors.lua)
-    color_overrides = {
-        vscLineNumber = '#FFFFFF',
-    },
+  -- Override colors (see ./lua/vscode/colors.lua)
+  color_overrides = {
+    vscLineNumber = '#FFFFFF',
+  },
 
-    -- Override highlight groups (see ./lua/vscode/theme.lua)
-    group_overrides = {
-        -- this supports the same val table as vim.api.nvim_set_hl
-        -- use colors from this colorscheme by requiring vscode.colors!
-        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
-    }
+  -- Override highlight groups (see ./lua/vscode/theme.lua)
+  -- this supports the same val table as vim.api.nvim_set_hl
+  -- use colors from this colorscheme by requiring vscode.colors!
+  group_overrides = {
+    -------- Rust --------
+    -- Mutable has underline
+    ['@lsp.typemod.variable.mutable.rust'] = { underline = true },
+    ['@lsp.typemod.parameter.mutable.rust'] = { underline = true },
+    ['@lsp.typemod.selfKeyword.mutable.rust'] = { underline = true },
+    -- Ownership is bold
+    ['@lsp.typemod.variable.consuming.rust'] = { bold = true, underdashed = true },
+    -- References have italic
+    ['@lsp.typemod.variable.reference.rust'] = { italic = true },
+    -- Type symbols
+    ['@lsp.type.interface.rust'] = { fg = c.vscYellowOrange },
+    ['@lsp.type.typeParameter.rust'] = { italic = true },
+    ['@lsp.type.enumMember.rust'] = { bold = true },
+    ['@lsp.type.macro.rust'] = { fg = c.vscPink },
+  }
 })
 require('vscode').load()
